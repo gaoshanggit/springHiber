4eef73f48d2e206345eca0f1c421ccb61db2ae1e
gaoshanggit <1058113237@qq.com>
2017-08-02 23:17:53 +0800

    springHibernate  demo

---
+++ src/main/java/com/gs/app/hiber/api/domain/User.java
@@ -0,0 +1,9 @@
+package com.gs.app.hiber.api.domain;
+
+/**
+ * @author GaoShan =.=
+ * @version 1.0
+ * @since 2017/8/2.22:54..
+ */
+public class User {
+}
---
+++ src/main/java/com/gs/app/hiber/impl/UserServiceImpl.java
@@ -0,0 +1,9 @@
+package com.gs.app.hiber.impl;
+
+/**
+ * @author GaoShan =.=
+ * @version 1.0
+ * @since 2017/8/2.23:07..
+ */
+public class UserServiceImpl {
+}
